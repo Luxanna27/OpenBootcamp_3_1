3076b589c4ac4b871c8d5b11ecdc342239a1c3aa
Luz <luxanna27@gmail.com>
2022-09-29 06:42:16 -0300

    .Crear una función con tres parámetros que sean números que se suman entre sí. .Llamar a la función en el main y darle valores.

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(suma (10, 15, 20));
+    }
+
+    public static int suma (int a, int b, int c) {
+        return a + b + c;
+    }
+}
